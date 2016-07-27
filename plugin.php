ad5d7a2037a6e1d8e4b9bca62574a25e35a03786
jaswsinc <jaswsinc@wsharks.com>
2016-07-27 12:36:52 -0800

    Initializing repo.

---
+++ plugin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin.
+ *
+ * @wp-plugin
+ *
+ * Version: 160727.10411
+ * Text Domain: woocommerce-kb-articles
+ * Plugin Name: WooCommerce KB Articles Pro
+ *
+ * Author: WP Sharks™
+ * Author URI: https://wpsharks.com
+ *
+ * License: GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Plugin URI: https://wpsharks.com/product/woocommerce-kb-articles-pro
+ * Description: WP Sharks™ plugin woocommerce-kb-articles.
+ */
+// PHP v5.2 compatible.
+
+if (!defined('WPINC')) {
+    exit('Do NOT access this file directly: '.basename(__FILE__));
+}
+require dirname(__FILE__).'/src/includes/wp-php-rv.php';
+
+if (require(dirname(__FILE__).'/src/vendor/websharks/wp-php-rv/src/includes/check.php')) {
+    require_once dirname(__FILE__).'/src/includes/plugin.php';
+} else {
+    wp_php_rv_notice('WooCommerce KB Articles Pro');
+}
---
+++ src/includes/classes/Base/Facades.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Facades.
+ *
+ * @author @jaswsinc
+ * @copyright WP Sharks™
+ */
+declare (strict_types = 1);
+namespace WebSharks\WpSharks\WooCommerceKBArticles\Pro\Classes\Base;
+
+use WebSharks\WpSharks\WooCommerceKBArticles\Pro\Classes;
+use WebSharks\WpSharks\WooCommerceKBArticles\Pro\Interfaces;
+use WebSharks\WpSharks\WooCommerceKBArticles\Pro\Traits;
+#
+use WebSharks\WpSharks\WooCommerceKBArticles\Pro\Classes\AppFacades as a;
+use WebSharks\WpSharks\WooCommerceKBArticles\Pro\Classes\SCoreFacades as s;
+use WebSharks\WpSharks\WooCommerceKBArticles\Pro\Classes\CoreFacades as c;
+#
+use WebSharks\WpSharks\Core\Classes as SCoreClasses;
+use WebSharks\WpSharks\Core\Interfaces as SCoreInterfaces;
+use WebSharks\WpSharks\Core\Traits as SCoreTraits;
+#
+use WebSharks\Core\WpSharksCore\Classes as CoreClasses;
+use WebSharks\Core\WpSharksCore\Classes\Core\Base\Exception;
+use WebSharks\Core\WpSharksCore\Interfaces as CoreInterfaces;
+use WebSharks\Core\WpSharksCore\Traits as CoreTraits;
+#
+use function assert as debug;
+use function get_defined_vars as vars;
+
+/**
+ * Pseudo-static facades.
+ *
+ * @since 160624.34776 Initial release.
+ */
+abstract class Facades
+{
+    use Traits\Facades\Foo;
+}
